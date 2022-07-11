30fae44e930776be8e4f195d3441cd9441171a9a
Tonycrz <tonybaidoo0@gmail.com>
2022-07-11 15:43:27 +0000

    semistandard

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -4,18 +4,16 @@ import _ from 'lodash';
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append("<p id='count'></p>")
+$('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-
-// let count = 0
 const updateCounter = () => {
   let count = $('#count').html() || 0;
   $('button').on('click', () => {
     count++;
     $('#count').html(`${count} clicks on the button`);
   });
-}
+};
 
 _.debounce(updateCounter, 500);
 updateCounter();
