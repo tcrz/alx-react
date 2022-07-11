3a939d2922c8b46cf1b897d2a59c6a0230b5e593
Tonycrz <tonybaidoo0@gmail.com>
2022-07-11 15:38:58 +0000

    change import style

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,6 +1,5 @@
-import $ from "jquery"
-
-const _ = require('lodash');
+import $ from 'jquery';
+import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
