3e34127985182f00d4412a9ee13e6bd84e70ca7c
Tonycrz <tonybaidoo0@gmail.com>
2022-07-11 15:45:47 +0000

    update

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -4,14 +4,14 @@ import _ from 'lodash';
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append("<p id='count'></p>");
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 const updateCounter = () => {
-  let count = $('#count').html() || 0;
+  let times = $('#count').html() || 0;
   $('button').on('click', () => {
-    count++;
-    $('#count').html(`${count} clicks on the button`);
+    times++;
+    $('#count').html(${times} clicks on the button);
   });
 };
 
