0783e798df29ff137baa836056569a909805d3a7
Tonycrz <tonybaidoo0@gmail.com>
2022-07-12 12:25:05 +0000

    updates

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -4,29 +4,14 @@ import './App.css';
 function App() {
   return (
     <div>
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} alt="logo"/>
         <h1>School dashboard</h1>
-      </header>
+      </div>
       <div className="App-body"><p>Login to access the full dashboard</p></div>
-      <footer className="App-footer"><p>Copyright 2020 - holberton School</p></footer>
+      <div className="App-footer"><p>Copyright 2020 - holberton School</p></div>
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+
   );
 }
 
