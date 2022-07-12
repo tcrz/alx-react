c3d3db8890dd5d22b950b27a2a41b9c25de2d2c0
Tonycrz <tonybaidoo0@gmail.com>
2022-07-12 16:05:35 +0000

    update

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -10,12 +10,13 @@ function App() {
         <img src={logo} alt="logo"/>
         <h1>School dashboard</h1>
       </div>
-      <div className="App-body"><p>Login to access the full dashboard</p></div>
-      <label for="email">Email</label>
-      <input type="text" id="email"/>
-      <label for="password">Password</label>
-      <input type="text" id="password"/>
-      <button>OK</button>
+      <div className="App-body"><p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input type="text" id="email"/>
+        <label htmlFor="password"> Password: </label>
+        <input type="text" id="password"/>
+        <button>OK</button>
+      </div>
       <div className="App-footer"><p>Copyright {getFullYear()} - {getFooterCopy(false)}</p></div>
     </div>
 
