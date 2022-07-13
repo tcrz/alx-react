aa767bfc0f685398db3183a319416a68c136a860
Tonycrz <tonybaidoo0@gmail.com>
2022-07-13 18:15:38 +0000

    task_3

---
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -0,0 +1,20 @@
+
+
+const wrapper = shallow(<App />);
+describe('App Component', () => {
+  it("renders without crashing", () => {
+    wrapper
+  })
+
+  it("App renders a div with the class: App-header", () => {
+   expect(wrapper.find('.App-header'))
+  })
+
+  it("App renders a div with the class: App-body", () => {
+   expect(wrapper.find('.App-body'))
+  })
+
+  it("App renders a div with the class: App-footer", () => {
+    expect(wrapper.find('.App-footer'))
+   })
+})
