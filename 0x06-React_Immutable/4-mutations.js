6e2f9d72e9ecef2f67c87e4a42ca3e9950409871
Tonycrz <tonybaidoo0@gmail.com>
2022-08-03 10:47:23 +0000

    [update] use withMutations function

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -4,18 +4,19 @@ Modify the value for the index 2, to Benjamin
 Modify the value for the index 4, to Oliver
 */
 
-const { Map } = require("immutable")
+const { Map } = require('immutable');
 
-const object =  {
-     1: 'Liam',
-     2: 'Noah',
-     3: 'Elijah',
-     4: 'Oliver',
-     5: 'Jacob',
-     6: 'Lucas',
+const object = {
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas'
+};
+
+export const map = Map(object);
+export const map2 = map.withMutations((map) => {
+  map.set(2, 'Benjamin').set(4, 'Oliver');
 }
-
-export const map = Map(object)
-export const map2 = map.set(2, 'Benjamin').set(4, 'Olivera')
-//console.log(map2.toJS())
-
+// console.log(map2.toJS())
