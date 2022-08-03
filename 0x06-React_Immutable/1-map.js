d5147f5efd8466c5e276ed7ad7f60c6251a9533a
Tonycrz <tonybaidoo0@gmail.com>
2022-08-03 10:19:48 +0000

    [fix] semistandard

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,6 +1,6 @@
 /* modify the function getImmutableObject using Map from Immutable.js */
 
-const Immutable = require("immutable")
-export default function getImmutableObject(object) {
-  console.log(Immutable.Map(object))
+const Immutable = require('immutable');
+export default function getImmutableObject (object) {
+  return Immutable.Map(object);
 }
