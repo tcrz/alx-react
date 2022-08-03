ebcd719751450f53f686e11d12108a4c7afdcb2f
Tonycrz <tonybaidoo0@gmail.com>
2022-08-03 10:30:04 +0000

    [update] change import

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,6 +1,6 @@
 /* modify the function getImmutableObject using Map from Immutable.js */
 
-const Immutable = require('immutable');
+import { Map } from 'immutable';
 export default function getImmutableObject (object) {
-  return Immutable.Map(object);
+  return Map(object);
 }
