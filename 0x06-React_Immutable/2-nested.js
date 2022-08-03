61374ca39ba0a23129946b741e6c63ec731231d7
Tonycrz <tonybaidoo0@gmail.com>
2022-08-03 11:27:53 +0000

    [update]: use fromJS function

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,9 +1,9 @@
 /* function should return the value of the object at the defined path */
-import { Map } from 'immutable';
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
   // make the object immutable
-  const ImmutableObject = Map(object);
+  const ImmutableObject = fromJS(object);
   // checks and returns any keys in the list can be found in ImmutableOject
   return ImmutableObject.getIn(array, undefined);
 }
