913f23c021f9fb23b7ead0d26cc6187d9981aec9
Tonycrz <tonybaidoo0@gmail.com>
2022-08-13 22:01:33 +0000

    [fix] uiReducer function and broken tests

--- 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/uiReducers.js
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/uiReducers.js
@@ -11,14 +11,14 @@ export const initialState = Map({
 export function uiReducer(state = initialState, action) {
   switch(action.type){
     case DISPLAY_NOTIFICATION_DRAWER:
-      return initialState.set("isNotificationDrawerVisible", true)
+      return state.set("isNotificationDrawerVisible", true)
     case HIDE_NOTIFICATION_DRAWER:
-      return initialState.set("isNotificationDrawerVisible", false)
+      return state.set("isNotificationDrawerVisible", false)
     case LOGIN_SUCCESS:
-      return initialState.set("isUserLoggedIn", true)
+      return state.set("isUserLoggedIn", true)
     case LOGIN_FAILURE: 
     case LOGOUT:
-      return initialState.set("isUserLoggedIn", false)
+      return state.set("isUserLoggedIn", false)
     default:
       return state
   }
