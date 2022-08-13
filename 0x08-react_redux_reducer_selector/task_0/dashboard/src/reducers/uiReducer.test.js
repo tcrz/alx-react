4f3d4517bad7f77b18c0afc548f0f55fb7ebee46
Tonycrz <tonybaidoo0@gmail.com>
2022-08-13 22:12:56 +0000

    [update] define test state once

--- 0x08-react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.test.js
@@ -2,6 +2,13 @@ import { initialState, uiReducer } from './uiReducers';
 import { SELECT_COURSE } from "../actions/courseActionTypes";
 import { LOGOUT, DISPLAY_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE } from '../actions/uiActionTypes'
 
+
+const state = {
+    isNotificationDrawerVisible: false,
+    isUserLoggedIn: true,
+    user: {}
+}
+
 describe("test uiReducer function", () => {
   it("(uiReducer function) returns state equals the initial state when no action is passed", ()=>{
     const currentState = uiReducer(initialState, {})
@@ -20,26 +27,15 @@ describe("test uiReducer function", () => {
   })
 
   it(`returns state changes isUserLoggedIn property correctly when action LOGIN_FAILURE is passed`, ()=>{
-    const initState = {
-      isNotificationDrawerVisible: false,
-      isUserLoggedIn: true,
-      user: {}
-    }
-    const currentState = uiReducer(initState, { type: LOGIN_FAILURE })
+    const currentState = uiReducer(state, { type: LOGIN_FAILURE })
     const expectedState = { isNotificationDrawerVisible: false, isUserLoggedIn: false, user: {} }
     expect(currentState).toEqual(expectedState)
   })
 
   it(`returns state changes isUserLoggedIn property correctly when action LOGOUT is passed`, () => {
-    const initState = {
-      isNotificationDrawerVisible: false,
-      isUserLoggedIn: true,
-      user: {}
-    }
-    const currentState = uiReducer(initState, { type: LOGOUT })
+    const currentState = uiReducer(state, { type: LOGOUT })
     const expectedState = { isNotificationDrawerVisible: false, isUserLoggedIn: false, user: {} }
     expect(currentState).toEqual(expectedState)
   })
-  
   
 })
